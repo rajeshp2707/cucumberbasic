471e451cf678744e0dba6786958c6c24c8985529
rajeshp2707 <162096605+rajeshp2707@users.noreply.github.com>
2024-03-08 16:15:51 +0530

    Update Hook.java

--- src/test/java/Steps/Hook.java
+++ src/test/java/Steps/Hook.java
@@ -20,12 +20,12 @@ public class Hook extends BaseUtil{
     @Before
     public void InitializeTest() {
 
-        System.out.println("Opening the browser : Chrome");
+        System.out.println("Opening the browser : Mock");
 
         //Passing a dummy WebDriver instance step info
-       // base.StepInfo = "FirefoxDriver";
-        System.setProperty("webdriver.chrome.driver","C:\\libs\\chromedriver.exe");
-        base.Driver= new ChromeDriver();
+       base.StepInfo = "FirefoxDriver";
+       // System.setProperty("webdriver.chrome.driver","C:\\libs\\chromedriver.exe");
+       // base.Driver= new ChromeDriver();
         
     }
 
